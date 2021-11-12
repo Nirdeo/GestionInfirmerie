b3950c41b2eae63fc4827b7d187a7adbdbf3370c
Victor De Domenico <victordedomenico.du60@gmail.com>
2021-11-12 16:26:18 +0100

    Ajout ConnexionBD
    Ajout ConnexionBD

--- InfirmerieDAL/ConnexionBD.cs
+++ InfirmerieDAL/ConnexionBD.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace InfirmerieDAL
 {
     public class ConnexionBD
     {
+        private static ConnexionBD uneConnexionBD;
+        private string chaineConnexion;
+        private SqlConnection maConnexion;
+
+        private ConnexionBD()
+        {
+        }
+
+        public string GetchaineConnexion()
+        {
+            return chaineConnexion;
+        }
+
+        public void SetchaineConnexion(string ch)
+        {
+            chaineConnexion = ch;
+        }
+
+        public static ConnexionBD GetConnexionBD()
+        {
+            if (uneConnexionBD == null)
+            {
+                uneConnexionBD = new ConnexionBD();
+            }
+
+            return uneConnexionBD;
+        }
+
+        public SqlConnection GetSqlConnection()
+        {
+            if (maConnexion == null)
+            {
+                maConnexion = new SqlConnection();
+            }
+
+            maConnexion.ConnectionString = chaineConnexion;
+
+            if (maConnexion.State == System.Data.ConnectionState.Closed)
+            {
+                maConnexion.Open();
+            }
+
+            return maConnexion;
+        }
+
+        public void CloseConnexion()
+        {
+            if (this.maConnexion != null && this.maConnexion.State != System.Data.ConnectionState.Closed)
+            {
+                this.maConnexion.Close();
+            }
+        }
     }
 }
