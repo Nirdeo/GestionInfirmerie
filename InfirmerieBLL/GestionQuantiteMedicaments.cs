d4c2d86f13f67c2460ad25bec8743953a6955ed3
Victor De Domenico <victordedomenico.du60@gmail.com>
2022-05-01 23:17:23 +0200

    PPE READY
    PPE READY

--- InfirmerieBLL/GestionQuantiteMedicaments.cs
+++ InfirmerieBLL/GestionQuantiteMedicaments.cs
@@ -1,11 +1,7 @@
-﻿using InfirmerieBO; // Référence la couche BO
-using InfirmerieDAL; // Référence la couche DAL
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using InfirmerieBO; // Référence la couche BO
+using InfirmerieDAL; // Référence la couche DAL
 
 namespace InfirmerieBLL
 {
